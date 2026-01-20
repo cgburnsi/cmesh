0ab3ad869b25c49035ac19da5baa98bd0ca67210
chris <christopher.g.burnside@gmail.com>
2026-01-19 22:25:01 -0600

    Completed Refactor For Core SnapMesh Codes (data_types and inputs)

--- core/parser.py
+++ core/parser.py
@@ -1,12 +1,48 @@
 ''' core/parser.py
-    --------------------
-    Read an input file
+    --------------
+    Recursive Descent Parser for the SnapMesh Input Format.
+    
+    This module consumes the stream of tokens from the Lexer and constructs
+    the structured NumPy arrays (Nodes, Faces, Fields) required by the core 
+    engine. It uses a "Dispatch Table" pattern to cleanly separate the logic 
+    for each section of the input file.
+
+    Attribution & License:
+    ----------------------
+    The recursive descent architecture used here is adapted from the 
+    "Teeny Tiny Compiler" project by Austen Hensley.  I have not used much of
+    his emitter code since I am sending data directly to the numpy arrays.
+    
+    Source: https://github.com/austenhensley
+    
+    Copyright (c) 2024 Austen Hensley
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 '''
+import sys
 import numpy as np
 from .data_types import NODE_DTYPE, FACE_DTYPE, CELL_DTYPE, CONSTRAINT_DTYPE, FIELD_DTYPE
-from .lexer import Lexer, TokenType # <--- Import your tool
+from .lexer import Lexer, TokenType 
 
 class Parser:
+    """
+    Parses the input file into structured data arrays.
+    
+    Architecture:
+    - Recursive Descent: The 'program' method calls 'process_section', which 
+      dispatches to specific handlers (e.g., 'parse_nodes').
+    - Dispatch Table: Section handlers are registered in 'self.section_handlers',
+      making it easy to add new features (like Materials or BCs) without 
+      breaking the core loop.
+    """
     def __init__(self, lexer):
         self.lexer = lexer
         self.curToken = None; self.peekToken = None
@@ -35,23 +71,29 @@ class Parser:
 
     # --- Core Parsing Helpers ---
     def checkToken(self, kind): return self.kind == kind
+    
     def nextToken(self):
         self.curToken = self.peekToken; self.peekToken = self.lexer.getToken()
         self.kind = self.curToken.kind if self.curToken else None
+        
     def match(self, kind):
         if not self.checkToken(kind): 
             got = self.curToken.text if self.curToken else 'None'
             self.abort(f"Expected {kind}, got {got}")
         self.nextToken()
-    def abort(self, msg): sys.exit(msg)
+        
+    def abort(self, msg): 
+        sys.exit(msg)
 
-    # --- Value Extraction Helpers (Cleans up the code significantly) ---
+    # --- Value Extraction Helpers ---
     def expect_int(self):
         val = int(self.curToken.text); self.match(TokenType.NUMBER)
         return val
+        
     def expect_float(self):
         val = float(self.curToken.text); self.match(TokenType.NUMBER)
         return val
+        
     def expect_enum(self, mapping_dict, default_val):
         val = default_val
         if self.checkToken(TokenType.IDENTIFIER):
@@ -94,21 +136,20 @@ class Parser:
         n1   = self.expect_int()
         n2   = self.expect_int()
         tag  = self.expect_int()
-        segs = self.expect_int() # Segments is now mandatory in logic
+        segs = self.expect_int() 
         self.data['faces'].append((fid, n1, n2, tag, segs))
 
     def parse_constraints(self):
         cid   = self.expect_int()
-        ctype = self.expect_enum(self.constraint_types, 1) # Default to line (1)
+        ctype = self.expect_enum(self.constraint_types, 1) 
         p1    = self.expect_float()
         p2    = self.expect_float()
         p3    = self.expect_float()
-        # Note: 'target' is currently hardcoded 0, can be updated later
         self.data['constraints'].append((cid, ctype, 0, p1, p2, p3))
 
     def parse_fields(self):
         sid   = self.expect_int()
-        stype = self.expect_enum(self.field_types, 0) # Default to global (0)
+        stype = self.expect_enum(self.field_types, 0)
         x1    = self.expect_float()
         y1    = self.expect_float()
         x2    = self.expect_float()
@@ -117,8 +158,6 @@ class Parser:
         self.data['fields'].append((sid, stype, x1, y1, x2, y2, v))
 
     def skip_section(self):
-        # Just consume the tokens for a line if needed, or implement generic skipper
-        # For now, we just consume the first token to avoid infinite loops if buggy
         self.nextToken() 
 
     def get_arrays(self):
@@ -129,8 +168,18 @@ class Parser:
             "fields":      np.array(self.data['fields'], dtype=FIELD_DTYPE)
         }
 
+# This is the public facing code for loading a data file
 def input_reader(filename):
     with open(filename, 'r') as f: 
-        parser = Parser(Lexer(f.read()))
+        lexer  = Lexer(f.read())
+        parser = Parser(lexer)
         parser.program()
+        
         return parser.get_arrays()
+    
+    
+    
+    
+    
+    
+    
